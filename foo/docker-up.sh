0ad3bc292806f0f5ffe67e4861bc04d061b012dc
Michael Martin <mwm.cal@gmail.com>
2015-05-05 11:52:18 -0700

    Updating with --rm

--- foo/docker-up.sh
+++ foo/docker-up.sh
@@ -1,1 +1,1 @@
-docker run -v ~/foo/bar:/bar -i -t sequenceiq/hadoop-docker:2.6.0 /etc/bootstrap.sh -bash
+docker run --rm -v ~/foo/bar:/bar -i -t sequenceiq/hadoop-docker:2.6.0 /etc/bootstrap.sh -bash
