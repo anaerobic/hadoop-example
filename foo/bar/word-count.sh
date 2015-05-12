ad306fdaf0913364769254cea99a6cd0f0e6c5b7
Michael Martin <mwm.cal@gmail.com>
2015-05-12 16:15:04 -0700

    using bin/hdfs instead of bin/hadoop script

--- foo/bar/word-count.sh
+++ foo/bar/word-count.sh
@@ -2,11 +2,11 @@ cd $HADOOP_PREFIX
 
 bin/hdfs dfsadmin -safemode leave
 
-bin/hadoop dfs -mkdir /hdfs-bar-bat
+bin/hdfs dfs -mkdir /hdfs-bar-bat
 
-bin/hadoop dfs -copyFromLocal /bar/bat/words /hdfs-bar-bat/words
+bin/hdfs dfs -copyFromLocal /bar/bat/words /hdfs-bar-bat/words
 
-bin/hadoop fs -rm -r /hdfs-bar-bat-output
+bin/hdfs fs -rm -r /hdfs-bar-bat-output
 
 bin/hadoop jar /bar/hadoop-example.jar foo.WordCount /hdfs-bar-bat /hdfs-bar-bat-output
 
